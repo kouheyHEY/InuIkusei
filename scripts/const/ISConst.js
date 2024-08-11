8658cfe91c869934dc926ebec2eb1c8128f0b977
kouheyHEY <vudtarm@gmail.com>
2024-08-11 23:58:19 +0900

    update const

--- scripts/const/ISConst.js
+++ scripts/const/ISConst.js
@@ -1,4 +1,29 @@
 const C_IS = {
 
+    /** キャラ１ 立ち絵 X座標 */
+    CHARA1_X: 64,
+    /** キャラ１ 立ち絵 Y座標 */
+    CHARA1_Y: 48,
+    /** キャラ１ 立ち絵 幅 */
+    CHARA1_W: 320,
+    /** キャラ１ 立ち絵 高さ */
+    CHARA1_H: 384,
 
+    /** キャラ１ ステータスウインドウ X座標 */
+    CHARA1_STATUS_WINDOW_X: 384,
+    /** キャラ１ ステータスウインドウ Y座標 */
+    CHARA1_STATUS_WINDOW_Y: 48,
+    /** キャラ１ ステータスウインドウ 幅 */
+    CHARA1_STATUS_WINDOW_W: 192,
+    /** キャラ１ ステータスウインドウ 高さ */
+    CHARA1_STATUS_WINDOW_H: 384,
+
+    /** メイン用テキストウインドウ X座標 */
+    WINDOW_TEXT_MAIN_X: 384,
+    /** メイン用テキストウインドウ Y座標 */
+    WINDOW_TEXT_MAIN_X: 384,
+    /** メイン用テキストウインドウ 幅 */
+    WINDOW_TEXT_MAIN_X: 384,
+    /** メイン用テキストウインドウ 高さ */
+    WINDOW_TEXT_MAIN_X: 384,
 }
